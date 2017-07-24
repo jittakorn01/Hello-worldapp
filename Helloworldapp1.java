8974487fc0ddfa0f7567cfcec5602b4eb9ade51f
jittakornSNRU <tlejittakorn@gmail.com>
2017-07-24 21:00:17 +0700

    เริ่มต้น Branch "Hello world"

--- Helloworldapp1.java
+++ Helloworldapp1.java
@@ -6,5 +6,9 @@ class HelloWorldApp {
     public static void main(String[] args) {
         System.out.println("Hello World!"); // Display the string.
         // Temporary comments
+        int i = 1;
+        wile(i<11){
+          system.out.println("Hello World"+i);
+      }
     }
 }
